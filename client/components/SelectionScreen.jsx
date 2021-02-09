80c668ad6172043ad395f6af03c11879800c6455
Steven Nguyen <steeven.nguyen1102@gmail.com>
2021-02-08 21:27:54 -0800

    got selection panels to start showing, need to add images to signify the different seasons

--- client/components/SelectionScreen.jsx
+++ client/components/SelectionScreen.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SelectionScreen.css';
+import SelectionPanel from './SelectionPanel.jsx';
 
 const SelectionScreen = () => {
+    const [seasons, setSeasons] = useState(['Winter', 'Spring', 'Summer', 'Fall']);
+
     return (
-        <div className="container-fluid-padding">
-            <div className="row text-center">
-                <div className="col-12">
-                    <h1 className="display-4">
-                        Seasons
-                    </h1>
+        <div>
+            <div className="container-fluid-padding">
+                <div className="row text-center">
+                    <div className="col-12">
+                        <h1 className="display-4">
+                            Seasons
+                        </h1>
+                        <hr />
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid-padding">
+                <div className="row text-center padding">
+                    {
+                        seasons.map((season, index) => (
+                            <SelectionPanel season={season} key={index}/>
+                        ))
+                    }
                 </div>
             </div>
         </div>
