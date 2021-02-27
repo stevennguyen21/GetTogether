5cca7fdee41b6737b74d68bf9133a0607fb4b95a
Steven Nguyen <steeven.nguyen1102@gmail.com>
2021-02-26 21:35:08 -0800

    add search bar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,11 +3,7 @@ import React, { useState } from 'react';
 const Navbar = () => {
     return (
         <div>
-            <nav className="navbar">
-                <div className="navbar-container">
-
-                </div>
-            </nav>
+            <input type="search" placeholder="Search city"></input>
         </div>
     )
 }
